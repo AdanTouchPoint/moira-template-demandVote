6cbe7747022978e49049fb8b4c9bbed2b9ff2bf1
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-10-19 15:26:05 -0600

    fix postalcode in leads endpoint2

--- app/assets/petitions/fetchLeads.js
+++ app/assets/petitions/fetchLeads.js
@@ -1,7 +1,7 @@
 import { fetchData } from "./fetchData"
 const fetchLeads = (successResponse, backendURLBase, endpoints, clientId, dataUser, emailData, allDataIn) =>{
         
-    fetchData('POST', backendURLBase, endpoints.toSaveLeads, clientId,`&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.postalcode ? dataUser.postalcode: "N/A"}&emailData=${dataUser.emailUser}&representative=${allDataIn}&emailMessage=${dataUser.text}&subject=${dataUser.subject}&sended=${successResponse}`)
+    fetchData('POST', backendURLBase, endpoints.toSaveLeads, clientId,`&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.postalCode ? dataUser.postalCode: "N/A"}&emailData=${dataUser.emailUser}&representative=${allDataIn}&emailMessage=${dataUser.text}&subject=${dataUser.subject}&sended=${successResponse}`)
     // axios.post(`https://payload-demo-tpm.herokuapp.com/leads?&firstName=${dataUser.userName ? dataUser.userName:''}&postalcode=${dataUser.zipCode ? dataUser.zipCode:dataUser.state}&emailData=${dataUser.emailUser}&representative=${emailData.name}&emailMessage=${dataUser.text}&city=${emailData.state}&party=${emailData.party}&clientId=${clientId}&sended=${respuestaDeExito}`, dataUseryEmail)
 }
 
